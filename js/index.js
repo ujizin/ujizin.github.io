026973e88787c0a81e09af91accc0bb6c2d334cd
ujizin <lucasyujideveloper@gmail.com>
2026-02-21 14:33:59 +0000

    Add GA4 tag and track social link clicks

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
 (($) => {
     'use strict'
 
+    const socialLinks = $.querySelectorAll('.social-network-list a[href]')
+
+    const trackSocialLinkClick = (link) => {
+        if (typeof window.gtag !== 'function') {
+            return
+        }
+
+        const socialNetwork = link.dataset.socialNetwork || 'unknown'
+        window.gtag('event', 'social_link_click', {
+            social_network: socialNetwork,
+            link_url: link.href,
+            outbound: true,
+            transport_type: 'beacon'
+        })
+    }
+
+    socialLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            trackSocialLinkClick(link)
+        })
+    })
 })(document)
